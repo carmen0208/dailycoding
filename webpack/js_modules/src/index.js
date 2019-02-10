dad2f5a2d5fa2c462b526959a334250c5aa6a91e
Carmen Liu <carmen.liujia@gmail.com>
2019-02-10 22:16:39 +1300

    babel loader for es6 syntax and import process

--- webpack/js_modules/src/index.js
+++ webpack/js_modules/src/index.js
@@ -1,4 +1,6 @@
-const sum = require('./sum')
-const total = sum(1, 2)
+import sum from './sum'
+import './image_viewer'
+
+const total = sum(10, 18)
 
 console.log(total)
--- webpack/js_modules/webpack.config.js
+++ webpack/js_modules/webpack.config.js
@@ -6,6 +6,20 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bindle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|ajv)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
 
