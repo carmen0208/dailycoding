e47fe689f019b8b3c27d1fdcb98ae86476093147
Carmen Liu <carmen.liujia@gmail.com>
2019-02-13 08:44:03 +1300

    using system.import to do dynamic import, not ideal as I want to use import not still not works. TODO

--- webpack/js_modules/src/image_viewer.js
+++ webpack/js_modules/src/image_viewer.js
@@ -2,14 +2,13 @@ import big from '../assets/large.jpg'
 import small from '../assets/small.png'
 
 import '../styles/image_viewer.css'
+export default () => {
+  const image = document.createElement('img')
+  // image.src = 'http://lorempixel.com/400/400'
+  image.src = small
+  document.body.appendChild(image)
 
-const image = document.createElement('img')
-// image.src = 'http://lorempixel.com/400/400'
-image.src = small
-
-document.body.appendChild(image)
-
-const bigImage = document.createElement('img')
-bigImage.src = big
-
-document.body.appendChild(bigImage)
+  const bigImage = document.createElement('img')
+  bigImage.src = big
+  document.body.appendChild(bigImage)
+}
--- webpack/js_modules/src/index.js
+++ webpack/js_modules/src/index.js
@@ -1,6 +1,18 @@
-import sum from './sum'
-import './image_viewer'
+// import sum from './sum'
+// import './image_viewer'
 
-const total = sum(10, 18)
+// const total = sum(10, 18)
 
-console.log(total)
+// console.log(total)
+
+
+const button = document.createElement('button')
+button.innerText = 'Click me'
+button.onclick = () => {
+  System.import('./image_viewer.js').then(module => {
+    console.log(module)
+    // module.default()
+  })
+}
+
+document.body.appendChild(button)
