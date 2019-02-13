32d90ec71eed5e96d6822933d90837e7dda05f4b
Carmen Liu <carmen.liujia@gmail.com>
2019-02-14 12:29:07 +1300

    pass the build for the webpack but it is too large

--- webpack/WebpackProject/webpack.config.js
+++ webpack/WebpackProject/webpack.config.js
@@ -1,10 +1,49 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
+const config = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
-};
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|ajv)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            },
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.css",
+      chunkFilename: "[id].css"
+    })
+  ]
+}
+
+module.exports = config
