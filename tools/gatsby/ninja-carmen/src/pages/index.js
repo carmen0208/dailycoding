6ba777e338fbcaaf45860dac507bc932d63c17b2
Carmen Liu <carmen.liujia@gmail.com>
2019-02-24 22:53:35 +1300

    get post list

--- tools/gatsby/ninja-carmen/src/pages/index.js
+++ tools/gatsby/ninja-carmen/src/pages/index.js
@@ -1,11 +1,51 @@
 import React from "react"
 import Header from '../components/Header'
+import { graphql } from 'gatsby'
 
-const Layout = () => {
+const Layout = ({data}) => {
+  const { edges } = data.allMarkdownRemark
+  console.log(edges)
   return (
     <div>
       <Header />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontFamily: 'avenir'
+      }}>
+        {edges.map(edge => {
+          const {frontmatter} = edge.node
+          return (
+            <div
+              key={frontmatter.path}
+              style={{marginBottom: '1rem'}}
+            >
+                {frontmatter.title}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
+
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark(
+      sort: {order: DESC, fields: [frontmatter___date]}
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
+
 export default Layout
