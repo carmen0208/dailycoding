6501519d98eb38e4b2b8c80b3a5d16f3827668d5
Carmen Liu <carmen.liujia@gmail.com>
2019-03-05 23:22:04 +1300

    1st learning of rxjs

---
+++ javascript/rxjs/rxjs-101/1.basic/basic.js
@@ -0,0 +1,23 @@
+import { from , Subscriber } from "rxjs"
+
+const observable$ = from([1, 2, 3, 4, 5])
+
+const subscriber = {
+  next: value => {
+    console.log(value)
+  },
+  complete: () => {
+    console.log("done")
+  },
+  error: value => {
+    console.log(value)
+  }
+}
+
+class DoubleSubscriber extends Subscriber {
+  _next(value) {
+    this.destination.next(value * 2 )
+  }
+}
+// observable$.subscribe(subscriber)
+observable$.subscribe(new DoubleSubscriber(subscriber))
