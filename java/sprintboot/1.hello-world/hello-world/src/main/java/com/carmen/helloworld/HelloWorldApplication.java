5ba90bb7334bb5be28ff4cf0a1e71e0a474df31a
Carmen Liu <carmen.liujia@gmail.com>
2019-04-19 16:10:30 +1200

    first springboot project

---
+++ java/sprintboot/1.hello-world/hello-world/src/main/java/com/carmen/helloworld/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.carmen.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello Spring";
+	}
+}
