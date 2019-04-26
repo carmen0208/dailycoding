966fb238d2723d24bc7fa3e551db804c35380640
Carmen Liu <carmen.liujia@gmail.com>
2019-04-26 20:33:51 +1200

    rxjs abservable

--- javascript/rxjs/try-rxjs/src/app/about/about.component.ts
+++ javascript/rxjs/try-rxjs/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {fromEvent, interval, timer} from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,41 +11,41 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // // multi value streams(emit multiply value and never complate)
+    // // callback hell if we want to make those 3 nest together
+    // // when click, then waiting for 3 seconds then start counts by 1 seconds
     // document.addEventListener('click', evt => {
     //   console.log(evt);
+    //   // single value stream(emit one value and complate)
+    //   setTimeout(() => {
+    //     console.log('Finished ......');
+    //     let counter = 0;
+    //     // multi value streams
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter ++ ;
+    //
+    //     }, 1000);
+    //   }, 3000);
     // });
-    //
-    // let counter = 0;
-    // // multi value streams
-    // setInterval(() => {
-    //   console.log(counter);
-    //   counter ++ ;
-    //
-    // }, 1000);
-    //
-    // // single value stream(emit one value and complate)
-    // setTimeout(() => {
-    //   console.log('Finished ......');
-    // }, 3000);
 
+    // vulctrl+shift+p get interval$ type is  Observable<number>
+    const interval$ = interval(1000);
 
-    // callback hell if we want to make those 3 nest together
-    // when click, then waiting for 3 seconds then start counts by 1 seconds
-    document.addEventListener('click', evt => {
-      console.log(evt);
-      // single value stream(emit one value and complate)
-      setTimeout(() => {
-        console.log('Finished ......');
-        let counter = 0;
-        // multi value streams
-        setInterval(() => {
-          console.log(counter);
-          counter ++ ;
-
-        }, 1000);
-      }, 3000);
+    interval$.subscribe(val => {
+      console.log('stream 1 =>' + val);
     });
+
+
+    const timerInterval$ = timer(3000, 1000);
+    timerInterval$.subscribe(val => {
+      console.log('stream 2 =>' + val);
+
+    });
+
+    const click$ = fromEvent(document, 'click');
+
+    click$.subscribe(evt => console.log(evt));
+
   }
 
 
