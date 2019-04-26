5b5d052de5a8343e8d137a0ab153957f7ce69a43
Carmen Liu <carmen.liujia@gmail.com>
2019-04-26 19:59:41 +1200

    what is streams of values

--- javascript/rxjs/try-rxjs/src/app/about/about.component.ts
+++ javascript/rxjs/try-rxjs/src/app/about/about.component.ts
@@ -10,6 +10,44 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // // multi value streams(emit multiply value and never complate)
+    // document.addEventListener('click', evt => {
+    //   console.log(evt);
+    // });
+    //
+    // let counter = 0;
+    // // multi value streams
+    // setInterval(() => {
+    //   console.log(counter);
+    //   counter ++ ;
+    //
+    // }, 1000);
+    //
+    // // single value stream(emit one value and complate)
+    // setTimeout(() => {
+    //   console.log('Finished ......');
+    // }, 3000);
+
+
+    // callback hell if we want to make those 3 nest together
+    // when click, then waiting for 3 seconds then start counts by 1 seconds
+    document.addEventListener('click', evt => {
+      console.log(evt);
+      // single value stream(emit one value and complate)
+      setTimeout(() => {
+        console.log('Finished ......');
+        let counter = 0;
+        // multi value streams
+        setInterval(() => {
+          console.log(counter);
+          counter ++ ;
+
+        }, 1000);
+      }, 3000);
+    });
   }
+
+
+
 
 }
