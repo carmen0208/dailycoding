cd9810ef7347b4db0144a7db294459bd881ce12a
Carmen Liu <carmen.liujia@gmail.com>
2019-04-26 16:23:51 +1200

    initial rxjs project

---
+++ javascript/rxjs/try-rxjs/src/app/course/course.component.ts
@@ -0,0 +1,54 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Course} from "../model/course";
+import {
+    debounceTime,
+    distinctUntilChanged,
+    startWith,
+    tap,
+    delay,
+    map,
+    concatMap,
+    switchMap,
+    withLatestFrom,
+    concatAll, shareReplay
+} from 'rxjs/operators';
+import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {Lesson} from '../model/lesson';
+
+
+@Component({
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit, AfterViewInit {
+
+
+
+    @ViewChild('searchInput') input: ElementRef;
+
+    constructor(private route: ActivatedRoute) {
+
+
+    }
+
+    ngOnInit() {
+
+        const courseId = this.route.snapshot.params['id'];
+
+
+
+    }
+
+    ngAfterViewInit() {
+
+
+
+
+    }
+
+
+
+
+}
