51703618b2826ac76759691e3da4f9ce70c2dfbe
Carmen Liu <carmen.liujia@gmail.com>
2019-04-27 21:23:20 +1200

    make call aync using chan(chan) go(go roution)

--- go/try-essential-go/06-channels/main.go
+++ go/try-essential-go/06-channels/main.go
@@ -15,16 +15,29 @@ func main() {
 		"http://github.com",
 	}
 
+	// channel to connect main and other go rountines
+	c := make(chan string)
+
 
 	for _, link := range links {
-		checkLnik(link)
+		//checkLnik(link)
+
+		// go roution
+		go checkLnik(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<- c)
 	}
 }
 
-func checkLnik(link string) {
+func checkLnik(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down, I think"
+		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Yes, it is Up!"
 }
