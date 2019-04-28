67bc11e661f5c0d2e0f0015fe0211e7e795056f4
Carmen Liu <carmen.liujia@gmail.com>
2019-04-28 14:06:16 +1200

    go template with variables

---
+++ go/web-go-101/005_template_data/01_data_string/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("005_template_data/01_data_int/tpl.gohtml"))
+}
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", `Release self-focus; embrace other-focus.`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
