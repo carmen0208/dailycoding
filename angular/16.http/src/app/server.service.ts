4bc46fe90a9201a0c3726d6d70ff5834d74d1f4f
Carmen Liu <carmen.liujia@gmail.com>
2019-05-12 23:27:39 +1200

    http example

---
+++ angular/16.http/src/app/server.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ServerService {
+  constructor(private http: Http) {}
+  storeServers(servers: any[]) {
+    const headers = new Headers({'Content-Type': 'application/json'});
+    // return this.http.post('https://udemy-ng-http.firebaseio.com/data.json',
+    //   servers,
+    //   {headers: headers});
+    return this.http.put('https://udemy-ng-http.firebaseio.com/data.json',
+      servers,
+      {headers: headers});
+  }
+  getServers() {
+    return this.http.get('https://udemy-ng-http.firebaseio.com/data')
+      .map(
+        (response: Response) => {
+          const data = response.json();
+          for (const server of data) {
+            server.name = 'FETCHED_' + server.name;
+          }
+          return data;
+        }
+      )
+      .catch(
+        (error: Response) => {
+          return Observable.throw('Something went wrong');
+        }
+      );
+  }
+  getAppName() {
+    return this.http.get('https://udemy-ng-http.firebaseio.com/appName.json')
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      );
+  }
+}
