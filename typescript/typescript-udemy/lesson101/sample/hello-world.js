e5471d49c03995b2d160a0350079d6a3870e4a63
Carmen Liu <carmen.liujia@gmail.com>
2019-05-19 22:51:57 +1200

    arrow function

--- typescript/typescript-udemy/lesson101/sample/hello-world.js
+++ typescript/typescript-udemy/lesson101/sample/hello-world.js
@@ -1,2 +1,20 @@
 var message = 'Hello World';
 console.log(message);
+function Person(name) {
+    this.name = name;
+    this.sayHi = function () {
+        // ths function would cause a error as the callback function()
+        // inside setTimeout will have another context,
+        // which means that if would check name from window context instead of the Person function
+        // it would cause a undefined error.
+        // setTimeout(function() {
+        //     console.log('Hello my name is ' + this.name);
+        // }, 1000)
+        var _this = this;
+        setTimeout(function () {
+            console.log('Hello my name is ' + _this.name);
+        }, 1000);
+    };
+}
+var person = new Person('Bob');
+person.sayHi();
--- typescript/typescript-udemy/lesson101/sample/hello-world.ts
+++ typescript/typescript-udemy/lesson101/sample/hello-world.ts
@@ -2,3 +2,22 @@
 let message = 'Hello World'
 
 console.log(message);
+
+function Person(name) {
+    this.name = name;
+    this.sayHi = function() {
+        // ths function would cause a error as the function() create another scope,
+        //which means that this.name is from window instead of the Person function
+        // it would cause a undefined error.
+        // setTimeout(function() {
+        //     console.log('Hello my name is ' + this.name);
+        // }, 1000)
+
+        setTimeout(() => {
+            console.log('Hello my name is ' + this.name);
+        },1000);
+    }
+}
+
+const person = new Person('Bob')
+person.sayHi();
