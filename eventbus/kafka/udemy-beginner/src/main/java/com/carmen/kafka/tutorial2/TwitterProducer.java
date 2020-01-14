3e5a25bee66f23b0c0f861fc198bc215bd4e137a
Carmen Liu <carmen.liujia@gmail.com>
2020-01-14 22:51:26 +1300

    safe producer

--- eventbus/kafka/udemy-beginner/src/main/java/com/carmen/kafka/tutorial2/TwitterProducer.java
+++ eventbus/kafka/udemy-beginner/src/main/java/com/carmen/kafka/tutorial2/TwitterProducer.java
@@ -90,6 +90,15 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+
+        // save producer configuration
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+
+        //
         return new KafkaProducer<>(properties);
     }
 
